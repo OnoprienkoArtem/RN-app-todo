d462cc583a0af907bdbe9f61d4699afc5a63e161
OnoprienkoArtem <o.artem.e@gmail.com>
2020-01-12 00:22:55 +0200

    create new simple project

---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export const Navbar = (props) => {
+    return (
+        <View style={styles.navbar}>
+            <Text style={styles.text}>Todo App</Text>
+        </View>
+    );
+}
+
+
+
+const styles = StyleSheet.create({
+    navbar: {
+
+
+    },
+    text: {
+
+    }
+});

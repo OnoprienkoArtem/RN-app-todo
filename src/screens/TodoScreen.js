988447837d1d988b6a76add1ef70135908b960b0
OnoprienkoArtem <o.artem.e@gmail.com>
2020-01-14 23:32:29 +0200

    add constant for color

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { THEME } from '../theme';
 
-export const TodoScreen = ({goBack, todo}) => {
+export const TodoScreen = ({ goBack, todo }) => {
     return (
         <View>
             <Text>{todo.textTodo}</Text>
-            <Button title='Back' onPress={goBack}></Button>
+            <View style={styles.buttons}>
+                <View style={styles.button}>
+                    <Button title='Back' color={THEME.GREY_COLOR} onPress={goBack} />
+                </View>
+                <View style={styles.button}>
+                    <Button title='Delete' color={THEME.DANGER_COLOR} />
+                </View>   
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    button: {
+        width: '40%'
+    }
 });

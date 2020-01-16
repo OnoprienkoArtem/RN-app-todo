880ad8e734caf062fa678024520f74140403c9dc
OnoprienkoArtem <o.artem.e@gmail.com>
2020-01-16 13:43:36 +0200

    add image when empty list

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Image } from 'react-native';
 
 import { AddTodo } from '../components/AddTodo';
 import { Todo } from '../components/Todo';
 
 export const MainScreen = ({ addTodo, removeTodo, todos, openTodo }) => {
+    let content = (
+        <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+                <Todo key={item.id} todo={item} onRemove={removeTodo} onOpen={openTodo} />
+            )}
+            keyExtractor={item => item.id.toString()}
+        />
+    )
+
+    if (todos.length === 0) {
+        content = (
+            <View style={styles.imgWrap}>
+                <Image style={styles.image} source={require('../../assets/no-items.png')}/>
+            </View>
+        )
+    }
     return (
         <View>
             <AddTodo onSubmit={addTodo} />
-            <FlatList
-                data={todos}
-                renderItem={({ item }) => (
-                    <Todo key={item.id} todo={item} onRemove={removeTodo} onOpen={openTodo} />
-                )}
-                keyExtractor={item => item.id.toString()}
-            />
+            {content}
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-
+    imgWrap: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+        height: 300,
+        marginTop: 20
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain'
+    }
 });
